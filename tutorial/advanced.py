890331359ea48ea14b36b91c87f494c405426f40
Madi <72963376+Madi-S@users.noreply.github.com>
2021-01-19 15:00:53 +0600

    adavanced helium usage added

--- tutorial/advanced.py
+++ tutorial/advanced.py
@@ -1,11 +1,28 @@
 from helium import *
+from time import sleep
 
 URL = 'https://egemen.kz/'
 
 def main():
-    start_firefox(URL, headless=True)
+    Config.implicit_wait_secs = 15
 
-    wait_until()
+    start_firefox(URL, headless=False)
+    menu = S('div.main-menu')       # selector
+    print(e.text for e in find_all(menu))
+
+    link = Link('Барлығы')
+    print(link.exists())
+    if link.exists():
+        print('Link found:', link.href)
+        highlight(link)
+        sleep(3)
+        click(link)
+
+    # box = ComboBox('қaзaқшa')
+    # print(box.value)
+    # select(box, 'english')
+
+    kill_browser()
 
 
 if __name__ == '__main__':
